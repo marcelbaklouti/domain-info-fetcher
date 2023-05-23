29b643d4d8fc9ea818c47e2f5098ff22eed0d163
marcelBaklouti <marcel@baklouti.de>
2023-05-23 14:23:50 +0200

    Add functions to retrieve HTTP status and format domain name. Renamed Interface IDomainInfo to DomainInfo

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,25 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [Unreleased]
+## [1.1.0] - 2023-05-23
+
+### Breaking
+
+- The `IDomainInfo` interface has been renamed to `DomainInfo` so the `fetchDomainInfo` function now returns a `DomainInfo` object instead of an `IDomainInfo` object.
+
+### Updated
+
+- Updated the README.md
+- Updated the package.json keywords
+- Updated the package.json description
+- Updated the installed packages to the latest version
+
+### Added
+
+- New HTTP Status Codes to the DomainInfo Object
+- Domains will now be formatted to lowercase before fetching the information
+- Domains that start with `http://`, `https://` or `www` will now be stripped of the protocol before fetching the information
+- New Tests for the new HTTP Status Codes
 
 ## [1.0.8] - 2023-03-19
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,18 +1,35 @@
 import { fetchDomainInfo } from '../index';
 
-describe('fetchDomainInfo', () => {
+describe('fetchDomainInfo function', () => {
   test('fetches domain info for baklouti.de', async () => {
     const domainInfo = await fetchDomainInfo('baklouti.de');
-
-    expect(domainInfo).toHaveProperty('sslData');
-    expect(domainInfo).toHaveProperty('serverData');
-    expect(domainInfo).toHaveProperty('dnsData');
-    expect(domainInfo?.dnsData).toHaveProperty('A');
-    expect(domainInfo?.dnsData).toHaveProperty('CNAME');
-    expect(domainInfo?.dnsData).toHaveProperty('TXT');
-    expect(domainInfo?.dnsData).toHaveProperty('MX');
-    expect(domainInfo?.dnsData).toHaveProperty('NS');
-    expect(domainInfo?.dnsData).toHaveProperty('SOA');
+    expect(domainInfo).toBeDefined();
   });
+
+  test('fetches domain info for https://baklouti.de', async () => {
+    const domainInfo = await fetchDomainInfo('https://baklouti.de');
+    expect(domainInfo).toBeDefined();
+  });
+
+  test('fetches domain info for invalid domain', async () => {
+    const domainInfo = await fetchDomainInfo('invalid.invalid');
+    expect(domainInfo).toBeUndefined();
+  });
+
+  test('fetches domain info for empty domain', async () => {
+    const domainInfo = await fetchDomainInfo('');
+    expect(domainInfo).toBeUndefined();
+  });
+
 });
 
+describe ('Values of DomainInfo are correct', () => {
+  test('fetches domain info for baklouti.de', async () => {
+    const domainInfo = await fetchDomainInfo('baklouti.de');
+    expect(domainInfo?.sslData).toBeDefined();
+    expect(domainInfo?.serverData).toBeDefined();
+    expect(domainInfo?.dnsData).toBeDefined();
+    expect(domainInfo?.httpStatus).toBeDefined();
+    expect(domainInfo?.httpStatus).toBeGreaterThan(0);
+  });
+});
